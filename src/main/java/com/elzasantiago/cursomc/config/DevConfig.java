67f029c90709d1ebb5a76908e51e40edc4447f7d
Elza Santiago <elzansantiago@gmail.com>
2019-01-22 18:53:19 -0200

    Implementando SmtpEmailService com o servidor do Google

--- src/main/java/com/elzasantiago/cursomc/config/DevConfig.java
+++ src/main/java/com/elzasantiago/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.elzasantiago.cursomc.services.DBService;
+import com.elzasantiago.cursomc.services.EmailService;
+import com.elzasantiago.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,5 +30,11 @@ public class DevConfig {
 	}
 	dbService.instatiateTestDatabase();
 	return true; 
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+		
 	}
 }
