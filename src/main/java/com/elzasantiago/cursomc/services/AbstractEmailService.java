1ac68b55ed12f66ce220b93558f7d8f8aa2ee090
Elza Santiago <elzansantiago@gmail.com>
2019-01-24 23:19:53 -0200

    Configuração inicial do Spring Security

--- src/main/java/com/elzasantiago/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/elzasantiago/cursomc/services/AbstractEmailService.java
@@ -15,36 +15,39 @@ import org.thymeleaf.context.Context;
 
 import com.elzasantiago.cursomc.domain.Pedido;
 
-public abstract class AbstractEmailService implements EmailService{
-
-	@Value("${default.sender}")
-	private String sender; 
+public abstract class AbstractEmailService implements EmailService {
 	
+	@Value("${default.sender}")
+	private String sender;
+
 	@Autowired
-	private  JavaMailSender javaMailSender;
+	private TemplateEngine templateEngine;
+
 	@Autowired
-	private  TemplateEngine templateEngine;
+	private JavaMailSender javaMailSender;
+
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
 		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
 		sendEmail(sm);
 	}
-
 	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
 		SimpleMailMessage sm = new SimpleMailMessage();
 		sm.setTo(obj.getCliente().getEmail());
 		sm.setFrom(sender);
-		sm.setSubject("Pedido confirmado Codigo: " + obj.getId());
+		sm.setSubject("Pedido confirmado! Código: " + obj.getId());
 		sm.setSentDate(new Date(System.currentTimeMillis()));
-		sm.setText(obj.toString()); 
+		sm.setText(obj.toString());
 		return sm;
 	}
+
 	protected String htmlFromTemplatePedido(Pedido obj) {
 		Context context = new Context();
 		context.setVariable("pedido", obj);
 		return templateEngine.process("email/confirmacaoPedido", context);
-		
 	}
+
+	@Override
 	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
 		try {
 			MimeMessage mm = prepareMimeMessageFromPedido(obj);
@@ -60,10 +63,9 @@ public abstract class AbstractEmailService implements EmailService{
 		MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
 		mmh.setTo(obj.getCliente().getEmail());
 		mmh.setFrom(sender);
-		mmh.setSubject("Pedido confirmado! Codigo: " + obj.getId());
+		mmh.setSubject("Pedido confirmado! Código: " + obj.getId());
 		mmh.setSentDate(new Date(System.currentTimeMillis()));
 		mmh.setText(htmlFromTemplatePedido(obj), true);
 		return mimeMessage;
-	}     
-}
- 
+	}
+} 
