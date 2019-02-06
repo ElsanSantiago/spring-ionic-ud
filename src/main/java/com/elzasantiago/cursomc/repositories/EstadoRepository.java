df0fb2ddb2de727f2ae5e241d0c6a69fbe71979a
Elza Santiago <elzansantiago@gmail.com>
2019-02-06 12:47:27 -0200

    Endpoints para buscar Estados e Cidades

--- src/main/java/com/elzasantiago/cursomc/repositories/EstadoRepository.java
+++ src/main/java/com/elzasantiago/cursomc/repositories/EstadoRepository.java
@@ -1,11 +1,16 @@
 package com.elzasantiago.cursomc.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.elzasantiago.cursomc.domain.Estado;
 
 @Repository
 public interface EstadoRepository extends JpaRepository<Estado, Integer>{
 
+	@Transactional(readOnly=true)
+	public List<Estado> findAllByOrderByNome();
 }
